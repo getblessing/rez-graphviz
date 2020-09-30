46c732a16986a408326d642061acc9713c92331c
David Lai <davidlatwe@gmail.com>
2020-09-30 22:18:34 +0800

    change deployment

--- package.py
+++ package.py
@@ -7,9 +7,15 @@ description = "Graphviz - Graph Visualization Software"
 
 url = "https://www.graphviz.org/"
 
-build_command = False
+variants = [
+    ["platform-*"],
+]
+
+
+private_build_requires = ["rezutil-1"]
+build_command = "python {root}/rezbuild.py {install}"
 
 
 def commands():
     env = globals()["env"]
-    env.PATH.append("c:/Graphviz/bin")
+    env.PATH.append("{root}/payload/bin")
